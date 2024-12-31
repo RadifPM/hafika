101855b4eba13451912a7d16c1538adb9892070b
RadifPM <radifpmofficial@gmail.com>
2025-01-01 01:06:54 +0700

    add

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById('heart').addEventListener('click', function() {
     this.classList.add('fade-out');
     
-    // Wait for the fade-out animation to complete before showing the Spotify iframe
+    // Wait for the fade-out animation to complete before showing the Spotify image
     setTimeout(function() {
-        document.getElementById('spotifyIframe').style.display = 'block';
+        document.getElementById('spotifyImage').style.display = 'block';
     }, 500); // Match this duration with the fade-out animation duration
 });
