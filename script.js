fe546c81197184245ae4a4a1499fa4b8cfcfcfc0
RadifPM <radifpmofficial@gmail.com>
2025-01-01 01:14:16 +0700

    add

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('heart').addEventListener('click', function() {
     setTimeout(function() {
         document.getElementById('spotifyImage').style.display = 'block';
         document.getElementById('textContainer').style.display = 'block';
-    }, 500); // Match this duration with the fade-out animation duration
+    }, 1000); // Match this duration with the fade-out animation duration (increased to 1000ms)
 });
 
 // Add event listener for the second line of text
