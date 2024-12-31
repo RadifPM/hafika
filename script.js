8a67b3545203ac4cf46fb65f839e04543fc6e43c
RadifPM <radifpmofficial@gmail.com>
2024-12-31 15:34:41 +0700

    ss

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.getElementById('heart').addEventListener('click', function() {
     this.classList.add('fade-out');
+    
+    // Wait for the fade-out animation to complete before showing the message
+    setTimeout(function() {
+        document.getElementById('proposalMessage').style.display = 'block';
+    }, 500); // Match this duration with the fade-out animation duration
 });
