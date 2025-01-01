408ee7c1755cba7f5553ce16a11f7bc3ff5e2e01
RadifPM <radifpmofficial@gmail.com>
2025-01-01 09:22:28 +0700

    tes akhir

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 document.getElementById('heart').addEventListener('click', function() {
     this.classList.add('fade-out');
     
-    // Wait for the fade-out animation to complete before showing the Spotify image and text
     setTimeout(function() {
         document.getElementById('spotifyImage').style.display = 'block';
         document.getElementById('textContainer').style.display = 'block';
-    }, 500); // Match this duration with the fade-out animation duration
+    }, 500);
 });
 
-// Add event listener for the second line of text
 document.getElementById('clickText').addEventListener('click', function() {
-    // Fade out the image and text
     document.getElementById('spotifyImage').classList.add('fade-out');
     document.getElementById('textContainer').classList.add('fade-out');
+
+    setTimeout(function() {
+        document.getElementById('responseContainer').style.display = 'block';
+    }, 500);
+});
+
+document.getElementById('yesBox').addEventListener('click', function() {
+    document.getElementById('responseContainer').classList.add('fade-out');
+    setTimeout(function() {
+        document.querySelector('.heart').style.display = 'block'; // Show heart again
+        document.querySelector('.heart').classList.remove ('fade-out');
+    }, 1000); // Match this duration with the fade-out animation duration
+});
+
+document.getElementById('noBox').addEventListener('mouseover', function() {
+    const randomX = Math.random() * (window.innerWidth - 100); // 100 is the width of the box
+    const randomY = Math.random() * (window.innerHeight - 50); // 50 is the height of the box
+    this.style.position = 'absolute';
+    this.style.left = randomX + 'px';
+    this.style.top = randomY + 'px';
 });
